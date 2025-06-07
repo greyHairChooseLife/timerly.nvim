4c12782c5b10e2f657d26d82981a04fb5c16558d
sangyeon kim <greyhairchooselife@gmail.com>
2025-06-07 18:47:17 +0900

    feat: add close command to Timerly
    - Timerly 플러그인에 close 기능 추가
    - close 커맨드 추가 (:TTimerlyClose)
    - 코드 스타일 개선 (require 구문 괄호 추가)
    - 들여쓰기 탭으로 통일

--- lua/timerly/init.lua
+++ lua/timerly/init.lua
@@ -1,107 +1,113 @@
-local utils = require "timerly.utils"
-local state = require "timerly.state"
+local utils = require("timerly.utils")
+local state = require("timerly.state")
 local api = vim.api
-local volt = require "volt"
-local volt_events = require "volt.events"
-local timerlyapi = require "timerly.api"
+local volt = require("volt")
+local volt_events = require("volt.events")
+local timerlyapi = require("timerly.api")
 
-state.ns = api.nvim_create_namespace "Timerly"
+state.ns = api.nvim_create_namespace("Timerly")
 
 local M = {}
 
 M.setup = function(opts)
-  state.config = vim.tbl_deep_extend("force", state.config, opts or {})
+	state.config = vim.tbl_deep_extend("force", state.config, opts or {})
 end
 
 M.open = function()
-  state.volt_set = true
-  local config = state.config
-  state.minutes = config.minutes[1]
+	state.volt_set = true
+	local config = state.config
+	state.minutes = config.minutes[1]
 
-  utils.secs_to_ascii(state.minutes * 60)
+	utils.secs_to_ascii(state.minutes * 60)
 
-  state.w = 24 + 2 + (2 * 4) + (2 * state.xpad)
-  state.w_with_pad = state.w - (2 * state.xpad)
+	state.w = 24 + 2 + (2 * 4) + (2 * state.xpad)
+	state.w_with_pad = state.w - (2 * state.xpad)
 
-  utils.open_watch(true)
-  state.is_center = true
-  -- utils.open_watch()
+	utils.open_watch(true)
+	state.is_center = true
+	-- utils.open_watch()
 
-  vim.fn.prompt_setcallback(state.input_buf, function(input)
-    local n = tonumber(input)
-    if type(n) == "number" then
-      state.minutes = n
-      timerlyapi.reset()
-    end
-  end)
+	vim.fn.prompt_setcallback(state.input_buf, function(input)
+		local n = tonumber(input)
+		if type(n) == "number" then
+			state.minutes = n
+			timerlyapi.reset()
+		end
+	end)
 
-  volt.gen_data {
-    {
-      buf = state.buf,
-      layout = require "timerly.layout",
-      xpad = state.xpad,
-      ns = state.ns,
-    },
-  }
+	volt.gen_data({
+		{
+			buf = state.buf,
+			layout = require("timerly.layout"),
+			xpad = state.xpad,
+			ns = state.ns,
+		},
+	})
 
-  volt.run(state.buf, { h = state.h, w = state.w })
-  volt_events.add(state.buf)
+	volt.run(state.buf, { h = state.h, w = state.w })
+	volt_events.add(state.buf)
 
-  volt.mappings {
-    bufs = { state.buf, state.input_buf },
-    after_close = function()
-      state.timer:stop()
-      state.buf = nil
-      state.input_buf = nil
-      state.volt_set = false
-    end,
-  }
+	volt.mappings({
+		bufs = { state.buf, state.input_buf },
+		after_close = function()
+			state.timer:stop()
+			state.buf = nil
+			state.input_buf = nil
+			state.volt_set = false
+		end,
+	})
 
-  require "timerly.actions"
+	require("timerly.actions")
 end
 
 M.toggle_watch = function()
-  if not state.volt_set then
-    M.open()
-  elseif state.visible_watch then
-    api.nvim_win_close(state.win, true)
-  else
-    utils.open_watch()
-  end
+	if not state.volt_set then
+		M.open()
+	elseif state.visible_watch then
+		api.nvim_win_close(state.win, true)
+	else
+		utils.open_watch()
+	end
 
-  state.visible_watch = not state.visible_watch
+	state.visible_watch = not state.visible_watch
 end
 
 M.toggle_input = function()
-  if not state.volt_set then
-    M.open()
-    utils.open_input()
-    state.visible_watch = true
-    state.visible_input = true
-  elseif state.visible_input then
-    api.nvim_win_close(state.input_win, true)
-    state.visible_input = false
-  elseif state.visible_watch then
-    utils.open_input()
-    state.visible_input = true
-  end
+	if not state.volt_set then
+		M.open()
+		utils.open_input()
+		state.visible_watch = true
+		state.visible_input = true
+	elseif state.visible_input then
+		api.nvim_win_close(state.input_win, true)
+		state.visible_input = false
+	elseif state.visible_watch then
+		utils.open_input()
+		state.visible_input = true
+	end
 end
 
 M.toggle = function()
-  if not state.volt_set then
-    M.open()
-    state.visible_watch = true
-  elseif state.volt_set and not state.visible_watch then
-    state.win = vim.api.nvim_open_win(state.buf, false, state.win_conf)
-    state.visible_watch = true
-    api.nvim_win_set_hl_ns(state.win, state.ns)
-    api.nvim_set_hl(state.ns, "Normal", { link = "ExdarkBg" })
-    api.nvim_set_hl(state.ns, "FLoatBorder", { link = "Exdarkborder" })
+	if not state.volt_set then
+		M.open()
+		state.visible_watch = true
+	elseif state.volt_set and not state.visible_watch then
+		state.win = vim.api.nvim_open_win(state.buf, false, state.win_conf)
+		state.visible_watch = true
+		api.nvim_win_set_hl_ns(state.win, state.ns)
+		api.nvim_set_hl(state.ns, "Normal", { link = "ExdarkBg" })
+		api.nvim_set_hl(state.ns, "FLoatBorder", { link = "Exdarkborder" })
+	else
+		api.nvim_win_hide(state.win)
+		state.visible_watch = false
+	end
+end
 
-  else
-    api.nvim_win_hide(state.win)
-    state.visible_watch = false
-  end
+M.close = function()
+	if state.volt_set and state.visible_watch then
+		api.nvim_win_hide(state.win)
+		state.visible_watch = false
+	end
 end
 
 return M
--- plugin/timerly.lua
+++ plugin/timerly.lua
@@ -1,3 +1,7 @@
 vim.api.nvim_create_user_command("TTimerlyToggle", function()
 	require("timerly").toggle()
 end, { desc = "Toggle Timerly" })
+
+vim.api.nvim_create_user_command("TTimerlyClose", function()
+	require("timerly").close()
+end, { desc = "Close Timerly" })
